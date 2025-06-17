adbaf040a232b43d29309bc8f546757e1be76473
Tim Sailer <timothysailer83@gmail.com>
2025-06-16 21:27:32 -0700

    class 1

---
+++ script.js
@@ -0,0 +1,61 @@
+console.log("Script");
+
+
+//strings
+let firstName = "Tim";
+let lastName = "Sailer";
+//numerical
+let age = 41;
+let height = 5.10; //in feet.inches
+
+//concantenation
+let fullName = firstName + " " + lastName; 
+console.log(fullName); //this line prints full name
+
+console.log(firstName);//this line prints specific variable such as firstName
+
+
+//operands
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2; //addition
+
+let difference = num2 - num1; //subtraction
+let product = num1 * num2; //multiplication
+let quotient = num2 / num1; //division
+let remainder = num2 % num1; //modulus
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);  
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+console.log("Remainder: " + remainder);
+
+//circle has radius of 3cm
+let radius = 3;
+let pi = 3.14; //approximate value of pi
+let area = pi * radius * radius; //area of circle formula
+
+console.log("Area of circle: " + area.toFixed(2) +"cm"); //prints area of circle
+
+//assignment wk1
+let adjective = 'incredible';
+let animal = 'Thylasine';
+let bestFriend = 'Carl';
+let food = 'Steak';
+let noun = 'airfoil';
+let pastVerb = 'kicked';
+let pluralNoun = 'buildings';
+let tvShow = 'Justified';
+let Teacher = 'Mrs. Putnam';
+let verb1 = 'punch';
+let verb2 = 'lick';
+console.log("The words I chose are: " + adjective + ", " + animal + ", " + bestFriend + ", " + food + ", " + noun + ", " + pastVerb + ", " + pluralNoun + ", " + tvShow + ", " + Teacher + ", " + verb1 + ", and " + verb2);
+console.log("And my story is as follows: ");
+console.log("Things That Drive Me Crazy");
+console.log("I just hate it when Mom serves " + food + " for dinner.");
+console.log("My pet " + animal + " chews my " + noun + " when I am not home.");
+console.log("when my best friend " + bestFriend + " decides to " + verb1 + "someone else.");
+console.log("I get " + pastVerb + " for something I didnt do.");
+console.log("Dad makes me wear " + pluralNoun + " to school.");
+console.log("My favorite TV show " + tvShow + " gets canceled because the station has to broadcast a news conference.");
+console.log("People " + verb2 +  " into my bedroom without knocking.");
